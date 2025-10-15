b3f1e8474ee5f0f3fb0eb2206537cf2f4c868c7c
hiideehoo <hiideeho@gmail.com>
2025-10-15 17:02:34 -0500

    second try

--- password.js
+++ password.js
@@ -1,10 +1,27 @@
 const readlineSync = require('readline-sync');
 
 let password;
+
+function hasLength(password) {
+    return password.length >= 8;
+}
+function hasUppercase(password) {
+    return /[A-Z]/.test(password);
+}
+function hasNumbers(password) {
+    return /\d/.test(password);
+}
+
 do {
-    password = readlineSync.question("Enter your password: ");
-    if (password!=="Jambalaya2025") {
-        console.log("Try again.");
+    password = readlineSync.question("Enter a password: ");
+    if (hasLength(password) === false) {
+        console.log("The password must be at least 8 characters long.")
     }
-} while (password!=="Jambalaya2025");
-console.log("Access granted.");
+    if (hasUppercase(password) === false) {
+        console.log("The password must contain at least one uppercase letter.")
+    }
+    if (hasNumbers(password) === false) {
+        console.log("The password must contain at least one number.")
+    }
+} while (hasLength(password) === false || hasUppercase(password) === false || hasNumbers(password) === false);
+console.log("Password accepted.");
